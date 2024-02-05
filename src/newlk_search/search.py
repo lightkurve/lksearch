67322099d127f68c8d10d4e26eb36f519f7c114a
Nschanche <nicole.e.schanche@nasa.gov>
2024-02-05 17:32:50 -0500

    Initial sketch of mission-specific search

--- src/newlk_search/search.py
+++ src/newlk_search/search.py
@@ -1,5 +1,7 @@
 class search():
 
+    # Shared functions that are used for searches by any mission
+
     def __init__():
 
     def search_timeseries():
@@ -11,6 +13,9 @@ class search():
 
     def search_all():
         # append search_TESS, search_Kepler, and search_K2 results
+
+    def sort():
+        # Basic sort
         
 
 
@@ -19,10 +24,19 @@ class search():
 
 class search_TESS(search):
 
-    def query_mast():
+    def get_ffi():
+        # Determines what sectors ffi data is in
 
+    def sort():
+        # base sort + TESS HLSP handling?
 
 class search_Kepler(search):
+
+    def fix_times():
+        # Fixes Kepler times
+
+    def handle_kbonus():
+        # Deals with kbonus-specific issues
 
 
 class search_K2(search):
