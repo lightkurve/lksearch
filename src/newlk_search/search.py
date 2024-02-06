890ae90d51f68b1a77702e34682f612855335382
Nschanche <nicole.e.schanche@nasa.gov>
2024-02-06 14:12:04 -0500

    Initial sketch of mission-specific search

--- src/newlk_search/search.py
+++ src/newlk_search/search.py
@@ -1,44 +1,76 @@
-class search():
+
+def search_timeseries(mission:['kepler','k2','tess'], quarter=None, campain=None, sector=None, hlsp=True):
+    # Append results from search_TESS, search_Kepler, and search_K2
+
+def search_cubedata(mission:['kepler','k2','tess'], quarter=None, campain=None, sector=None, hlsp=True):
+
+
+class _search_mission():
 
     # Shared functions that are used for searches by any mission
 
     def __init__():
+        if 'Kepler' in mission:
+            return 
 
-    def search_timeseries():
+    def __repr__():
 
-    def search_cubedata():
+    def _search_timeseries():
 
-    def query_mast():
+    def _search_cubedata():
+
+    def _query_mast():
         # Constructs the appropriate query
 
-    def search_all():
-        # append search_TESS, search_Kepler, and search_K2 results
-
-    def sort():
+    def _sort():
         # Basic sort
-        
+
+    def _parse_name():
+
+    def _download_one():
+
+    def download():
+
+    
 
 
 
+class search_TESS(search, hlsp=False):
 
+     def search_cubedata():
+         _cubedata + _get_ffi
 
-class search_TESS(search):
-
-    def get_ffi():
+    def _get_ffi():
         # Determines what sectors ffi data is in
 
     def sort():
         # base sort + TESS HLSP handling?
 
-class search_Kepler(search):
+    def download_ffi():
 
+    def filter_hlsp():
+
+
+    
+class search_Kepler(search, hlsp=False):
+
+    def search_cubedata():
+        # Regular _search_cubedata + processing
     def fix_times():
         # Fixes Kepler times
 
     def handle_kbonus():
         # Deals with kbonus-specific issues
 
+    def get_sequence_number():
 
-class search_K2(search):
+
+class search_K2(search, hlsp=False):
+
+    def search_cubedata():
+        # Regular _search_cubedata + processing
+
+    def parse_split_campaigns():
+
 
 
