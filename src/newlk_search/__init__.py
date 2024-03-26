97d262f93584f6eefe53f8d1e5efd9a27e9f6d60
Tyler Pritchard <tylerapritchard@gmail.com>
2024-03-26 14:01:40 -0400

    updates

--- src/newlk_search/__init__.py
+++ src/newlk_search/__init__.py
@@ -5,6 +5,9 @@ from . import config as _config
 import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
+
+from . import __version__
+
 class Conf(_config.ConfigNamespace):
     """
     Configuration parameters for `search`.
