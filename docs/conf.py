157d586b5fdc0f969af6cb25ea2203aa88eb39c6
Tyler Pritchard <tylerapritchard@gmail.com>
2024-04-26 02:46:21 -0400

    ci

--- docs/conf.py
+++ docs/conf.py
@@ -20,9 +20,12 @@ sys.path.insert(0, os.path.abspath("../../src"))
 for x in os.walk("../../src"):
     sys.path.insert(0, x[0])
 print(sys.path)
-
-import tssc as tssc
-from tssc import MASTSearch, TESSSearch, K2Search, KeplerSearch
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath('../src/tssc'))
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../src/'))
+sys.path.insert(0, os.path.abspath('../../src/tssc'))
 
 project = "Timeseries Simple Search Combo (tssc)"
 copyright = "2024, TESS Science Support Center"
