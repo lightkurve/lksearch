0a43c51b771c5fa134f8385f9e0bd7e04dd96284
Tyler Pritchard <tylerapritchard@gmail.com>
2024-04-26 02:19:03 -0400

    conf.py update ci v3

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,10 @@ except ImportError:
     print("sphinx_astropy not found, default configuration will be used.")
 import os
 import sys
-#import tssc
-from tssc import *
+
+for x in os.walk('../../src'):
+    sys.path.insert(0, x[0])
+
 # Add the modules source to the path
 sys.path.insert(0, os.path.abspath("../../src"))
 
