f05ec191b9d9f622fbf55b943ba2486ebcb1a9fb
Tyler Pritchard <tylerapritchard@gmail.com>
2024-12-05 01:21:58 -0500

    added alternative name query/match tests

---
+++ tests/test_catalogs_names.py
@@ -0,0 +1,22 @@
+""" Tests alternate name lookups and matching """
+from lksearch.catalogsearch import (
+    query_names,
+    match_names_catalogs,
+)
+import pytest
+
+
+def test_query_names():
+    res = query_names("Gaia DR3 3796414192429498880")
+    assert "EPIC 201563164" in res.id.values
+
+    list_res = query_names(["Kepler 10", "Kepler 16"])
+    assert len(list_res) == 2
+
+
+def test_match_names_catalogs():
+    res = match_names_catalogs(["Kepler 10", "Kepler 16"], match=["tic", "kic", "epic"])
+
+    assert len(res.columns) == 4
+    assert len(res["search"]) == 2
+    assert res["epic"][0] == ""
