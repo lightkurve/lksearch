5ef7e7b29ac9c12ee3327d3ab7a18c3b61805429
Nschanche <nicole.e.schanche@nasa.gov>
2024-12-19 09:55:38 -0500

    added tesscut test

--- src/lksearch/TESSSearch.py
+++ src/lksearch/TESSSearch.py
@@ -91,7 +91,7 @@ class TESSSearch(MASTSearch):
         pipeline = np.atleast_1d(pipeline)
 
         # Sending 'pipeline' to MASTSearch init causes a SearchError, so handle that separately
-        if "tesscut" in [p.lower() for p in pipeline] or (type(pipeline) is type(None)):
+        if (type(pipeline) is type(None)) or ("tesscut" in [p.lower() for p in pipeline]) :
             add_tesscut = True
             pipeline = np.delete(
                 pipeline, [p.lower() for p in pipeline].index("tesscut")
